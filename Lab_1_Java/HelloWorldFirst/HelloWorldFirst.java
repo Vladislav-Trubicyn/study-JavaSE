98b02ed847b7774f78873643021086c78b99b817
Vladislav-Trubicyn <79567861+Vladislav-Trubicyn@users.noreply.github.com>
2021-02-24 12:11:35 +0400

    Add files via upload

---
+++ Lab_1_Java/HelloWorldFirst/HelloWorldFirst.java
@@ -0,0 +1,5 @@
+class HelloWorldFirst {
+    public static void main(String[] args) {
+        System.out.println("Hello from first World! and Vladislav");
+    }
+}
---
+++ Lab_1_Java/HelloWorldSecond/HelloWorldSecond.java
@@ -0,0 +1,5 @@
+class HelloWorldSecond {
+    public static void main(String[] args) {
+        System.out.println("Hello from second World! and Vladislav");
+    }
+}
