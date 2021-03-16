0f86b586ebd3080734fad26f76936d4814e1bf39
Vladislav-Trubicyn <79567861+Vladislav-Trubicyn@users.noreply.github.com>
2021-03-16 18:37:28 +0400

    Add files via upload

--- JavaObject/src/com/JavaObject/Main.java
+++ JavaObject/src/com/JavaObject/Main.java
@@ -1,16 +1,15 @@
 package com.JavaObject;
 
-import java.sql.SQLOutput;
-
 public class Main
 {
     public static void main(String[] args)
     {
         ExtClass ec1 = new ExtClass((byte)1,2,3,"4");
-        ExtClass ec2 = new ExtClass((byte)1,2,3,"44");
+        ExtClass ec2 = new ExtClass((byte)1,2,3,"4");
 
-        System.out.println(ec1.equals(ec2));
-
+        System.out.println("Одинаковы ли объекты " + ec1.equals(ec2));
+        System.out.println("Хэш сумма первого объекта " + ec1.hashCode() + " вторго объекта " + ec2.hashCode());
+        System.out.println("Конверn в строку " + ec1.toString());
     }
 
 }
